fe9e58b81d36c5354d416d9fc0225863c225e05f
Carter907 <laptopmemez@gmail.com>
2022-10-06 19:40:21 -0400

    Math Practice commit
    null

---
+++ MathPractice/MathPractice.java
@@ -0,0 +1,15 @@
+package MathPractice;
+
+public class MathPractice {
+    public static void main(String[] var0) {
+        System.out.println("The square root of 30 is " + Math.sqrt(30.0));
+        System.out.println("The sin of 100 radians is " + Math.sin(100.0));
+        System.out.println("The sin of 100 degrees is " + Math.sin(Math.toRadians(100.0)));
+        System.out.println("The cos of 100 radians is " + Math.cos(100.0));
+        System.out.println("The cos of 100 degrees is " + Math.cos(Math.toRadians(100.0)));
+        System.out.println("44.7 rounded to the nearest integer is " + Math.round(44.7));
+        System.out.println("The maximum of char K and 70 is " + Math.max(75, 70));
+        System.out.println("The minimum of char K and 70 is " + Math.min(75, 70));
+        System.out.println("7 to the eighth power is " + Math.pow(7.0, 8.0));
+    }
+}
